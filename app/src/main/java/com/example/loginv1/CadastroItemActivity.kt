7f34171461c98c456d84f851dd8e2bc15558adf8
Guilherme Viana <guilhermevp1996@hotmail.com>
2022-09-07 16:21:21 -0300

    Inclusão da activity de Cadastro de despesas V1

--- app/src/main/java/com/example/loginv1/CadastroItemActivity.kt
+++ app/src/main/java/com/example/loginv1/CadastroItemActivity.kt
@@ -1,39 +1,71 @@
 package com.example.loginv1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class CadastroItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_item)
-
+        //Spinner do campo Categoria
         val spin_Categoria: Spinner = findViewById(R.id.spin_Categoria)
-        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
             this,
             R.array.spin_categorias,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
             spin_Categoria.adapter = adapter
         }
-
+        //Spinner do campo Status
         val spin_Status: Spinner = findViewById(R.id.spin_Status)
-        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
             this,
             R.array.spin_status,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
             spin_Status.adapter = adapter
+        }
+        val email_user = Firebase.auth.currentUser!!.email
+        val txt_Categoria = spin_Categoria.selectedItem.toString()
+        val txt_Status = spin_Status.selectedItem.toString()
+        val edt_Valor = findViewById<EditText>(R.id.edt_Valor)
+        val edt_DataVencimento = findViewById<EditText>(R.id.edt_DataVencimento)
+        val edt_Descricao = findViewById<EditText>(R.id.edt_Descicao)
+        val btn_Adicionar = findViewById<FloatingActionButton>(R.id.btn_Adicionar)
+        val btn_CancelarCadastro = findViewById<FloatingActionButton>(R.id.btn_CancelarCadastro)
+
+        btn_Adicionar.setOnClickListener{
+            val db = Firebase.firestore
+            val despesa = hashMapOf(
+                "categoria" to txt_Categoria,
+                "status" to txt_Status,
+                "valor" to edt_Valor.text.toString().toInt(),
+                "dataVencimento" to edt_DataVencimento.text.toString(),
+                "descricao" to edt_Descricao.text.toString()
+            )
+            db.collection(email_user.toString())
+                .add(despesa)
+                .addOnSuccessListener {
+                    Toast.makeText(this,"Armazenamento concluído", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,PrincipalActivity::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this,"Falha no armazenamento: "+edt_Descricao.text.toString(),Toast.LENGTH_SHORT).show()
+                }
+        }
+
+        btn_CancelarCadastro.setOnClickListener{
+            val intent = Intent(this,PrincipalActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/loginv1/PrincipalActivity.kt
+++ app/src/main/java/com/example/loginv1/PrincipalActivity.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.util.*
+import kotlin.collections.HashMap
 
 class PrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +24,12 @@ class PrincipalActivity : AppCompatActivity() {
 
         val username = auth.currentUser!!.displayName
         txv_User.text = "Olá,$username"
+        val recycleview_Despesas = findViewById<RecyclerView>(R.id.recyclerView_Despesas)
+        recycleview_Despesas.layoutManager = LinearLayoutManager(this)
+        recycleview_Despesas.setHasFixedSize(true)
+
 
         val btn_AddItem = findViewById<FloatingActionButton>(R.id.btn_AddItem)
-
 
         btn_AddItem.setOnClickListener(){
             val intent = Intent(this,CadastroItemActivity::class.java)
