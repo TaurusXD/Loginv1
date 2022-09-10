3fc5538f2b2a43cc472a36cf2feba5721fb38ca7
Guihayashi <guilherme-hayashi@hotmail.com>
2022-09-09 21:43:29 -0300

    Atualização da activity de cadastro e inclusão de título na activity de login
    Adição de proteção contra campos vazios e verificação de repetição de senha

--- app/src/main/java/com/example/loginv1/CadastroActivity.kt
+++ app/src/main/java/com/example/loginv1/CadastroActivity.kt
@@ -24,27 +24,42 @@ class CadastroActivity : AppCompatActivity() {
         val edt_NomeCad = findViewById<TextView>(R.id.edt_NomeCad)
         val edt_EmailCad = findViewById<TextView>(R.id.edt_EmailCad)
         val edt_SenhaCad = findViewById<TextView>(R.id.edt_SenhaCad)
+        val edt_RSenhaCad = findViewById<TextView>(R.id.edt_RSenhaCad)
         val btn_Cadastrar = findViewById<Button>(R.id.btn_Cadastrar)
 
-        btn_Cadastrar.setOnClickListener{
+        btn_Cadastrar.setOnClickListener {
             val cadEmail = edt_EmailCad.text.toString()
             val cadSenha = edt_SenhaCad.text.toString()
+            val cadRSenha = edt_RSenhaCad.text.toString()
             val cadNome = edt_NomeCad.text.toString()
-            auth.createUserWithEmailAndPassword(cadEmail, cadSenha)
-                .addOnSuccessListener {
-                    val currentUser = auth.currentUser
-                    val userProfileChangeRequest = UserProfileChangeRequest.Builder().setDisplayName(cadNome).build()
-                    Toast.makeText(this,"Usuário ${currentUser.toString()}", Toast.LENGTH_SHORT).show()
-                    currentUser!!.updateProfile(userProfileChangeRequest)
-                        .addOnCompleteListener{
-                            startActivity(Intent(this,MainActivity::class.java))
-                        }
-                }
-                .addOnFailureListener{
-                    Toast.makeText(this,"${auth.currentUser}", Toast.LENGTH_SHORT).show()
-                }
 
+            if (cadEmail.toString() == "" || cadSenha.toString() == "" || cadRSenha.toString() == "" || cadNome.toString() == "") {
+                Toast.makeText(this, "Campos Requiridos", Toast.LENGTH_SHORT).show()
+            }
+            else if(cadSenha.toString() != cadRSenha.toString()){
+                Toast.makeText(this,"As senhas não conferem", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                auth.createUserWithEmailAndPassword(cadEmail, cadSenha)
+                    .addOnSuccessListener {
+                        val currentUser = auth.currentUser
+                        val userProfileChangeRequest =
+                            UserProfileChangeRequest.Builder().setDisplayName(cadNome).build()
+                        Toast.makeText(
+                            this,
+                            "Usuário ${currentUser.toString()} criado com sucesso",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        currentUser!!.updateProfile(userProfileChangeRequest)
+                            .addOnCompleteListener {
+                                startActivity(Intent(this, MainActivity::class.java))
+                            }
+
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "${auth.currentUser}", Toast.LENGTH_SHORT).show()
+                    }
+            }
         }
-
     }
 }
